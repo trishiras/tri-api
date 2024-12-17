f7f1c23aa44f86be31c5966be6842ee1413ee08a
sumit <sumitsingh12013@gmail.com>
2024-12-17 06:33:15 +0000

    Added new create token api and updated get task.

--- tri_api/routes/super/tasks.py
+++ tri_api/routes/super/tasks.py
@@ -1,8 +1,14 @@
+import secrets
 from tri_api.models.tenant.user import User
-from tri_api.models.tenant.tenant import ActiveTokenList
 from fastapi import Query, APIRouter, Response, HTTPException
-from tri_api.celery.tasks import populate_trove_intel_database
 from tri_api.support.current_super_user import current_super_user
+from tri_api.models.tenant.tenant import (
+    InviteToken,
+    ActiveTokenList,
+)
+from tri_api.celery.tasks import (
+    populate_trove_intel_database,
+)
 from tri_api.models.scanner.task import (
     ScannerTask,
     ScannerTaskBase,
@@ -30,7 +36,7 @@ router = APIRouter(
 @router.get(RouteEnum.populate_database.value)
 async def database_update(super_user_login: SuperUserLogin) -> Response:
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
@@ -43,10 +49,32 @@ async def database_update(super_user_login: SuperUserLogin) -> Response:
     return Response(status_code=200)
 
 
+@router.post(RouteEnum.token.value)
+async def create_token(super_user_login: SuperUserLogin):
+
+    super_user = await current_super_user(
+        super_user_login=super_user_login,
+    )
+
+    existing_token = await InviteToken.find(
+        InviteToken.status == True,
+        InviteToken.user == None,
+    ).to_list()
+
+    number_of_tokens_to_create = 10 - len(existing_token)
+
+    for _ in range(number_of_tokens_to_create):
+        token = secrets.token_urlsafe(9)
+        invite_token = InviteToken(token=token)
+        await invite_token.create()
+
+    return Response(status_code=200)
+
+
 @router.get(RouteEnum.token.value, response_model=ActiveTokenList)
 async def get_active_tokens(super_user_login: SuperUserLogin) -> ActiveTokenList:
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
@@ -56,7 +84,7 @@ async def get_active_tokens(super_user_login: SuperUserLogin) -> ActiveTokenList
 @router.get(RouteEnum.user.value, response_model=list[User])
 async def get_users(super_user_login: SuperUserLogin) -> list[User]:
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
@@ -74,7 +102,7 @@ async def update_member(
         login_key=super_user_member_update.login_key,
     )
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
@@ -91,19 +119,32 @@ async def update_member(
 )
 async def get_tasks_list(
     super_user_login: SuperUserLogin,
+    status: str = Query(None),
+    synced: bool = Query(False),
     page: int = Query(1, ge=1),
     page_size: int = Query(100, ge=1, le=100),
 ) -> PaginatedScannerTaskResponse:
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
     skip = (page - 1) * page_size
 
-    total_tasks = await ScannerTask.find().count()
+    total_tasks = await ScannerTask.find(
+        ScannerTask.status == status,
+        ScannerTask.synced == synced,
+    ).count()
 
-    tasks = await ScannerTask.find().skip(skip).limit(page_size).to_list()
+    tasks = (
+        await ScannerTask.find(
+            ScannerTask.status == status,
+            ScannerTask.synced == synced,
+        )
+        .skip(skip)
+        .limit(page_size)
+        .to_list()
+    )
 
     return {
         "total": total_tasks,
@@ -130,9 +171,10 @@ async def update_task(
         status=scanner_task_update.status,
         result_url=scanner_task_update.result_url,
         status_message=scanner_task_update.status_message,
+        synced=scanner_task_update.synced,
     )
 
-    super_user = current_super_user(
+    super_user = await current_super_user(
         super_user_login=super_user_login,
     )
 
